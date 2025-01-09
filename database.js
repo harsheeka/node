4228f56908b8f7e5dd6297f2538d5098b1f13830
harsheeka <yoyoharshikaa@gmail.com>
2025-01-09 22:55:27 +0530

    creating a server using mongodb

--- database.js
+++ database.js
@@ -1,1 +1,24 @@
-const URI = "mongodb+srv://harshika:zal4qrG5Q059tQ3r@nodejsproject.i310n.mongodb.net/?retryWrites=true&w=majority&appName=nodeJsProject";
+const {MongoClient} = require("mongodb");  //importing mongodb  from node.js driver
+
+const url = "mongodb+srv://harshika:zal4qrG5Q059tQ3r@nodejsproject.i310n.mongodb.net/?retryWrites=true&w=majority&appName=nodeJsProject";
+const client = new MongoClient(url); //creating instantce of mongodb
+
+const dbName = 'nodejsproject';  //name of database we want to work upon
+
+async function main() {
+    await client.connect(); //connecting with mongodb
+    console.log('Connected successfully to server');
+
+
+    const db = client.db(dbName);
+    const collection = db.collection('users');
+
+    return 'done.';
+    
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
+
